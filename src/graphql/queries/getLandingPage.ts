43bc7a9539de16ff97da3fe78962979796c85936
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-05-14 20:40:07 -0300

    feat: add header query

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,14 +1,34 @@
 import { gql } from 'graphql-request'
 
 const GET_LANDING_PAGE = gql`
-  {
-    landingPage {
-      logo {
-        alternativeText
-        url
-      }
+fragment logo on LandingPage {
+  logo {
+    alternativeText
+    url
+  }
+}
+
+fragment header on LandingPage {
+  header {
+    title
+    description
+    button {
+      label
+      url
+    }
+    image {
+      alternativeText
+      url
     }
   }
+}
+
+query GET_LANDING_PAGE {
+  landingPage {
+    ...logo
+    ...header
+  }
+}
 `
 
 export default GET_LANDING_PAGE
